ee6d7f1981543f7f3dc155b0c3af240085039742
OlleHellsten <olle411@hotmail.com>
2025-01-10 14:12:21 +0100

    updated the Highscore class

--- src/main/java/Highscore.java
+++ src/main/java/Highscore.java
@@ -1,4 +1,23 @@
-//Olle
-public class Highscore {
+import java.util.*;
 
+public class Highscore {
+    private TreeMap<Integer, String> scores;
+
+    public Highscore() {
+        scores = new TreeMap<>((Collections.reverseOrder()));
+
+    }
+    public void addScore(String name, int score) {
+        scores.put(score, name);
+    }
+
+
+    public void displayTopScores() {
+        System.out.println("Top 5 Scores:");
+        int count = 0;
+        for (Map.Entry<Integer, String> entry : scores.entrySet()) {
+            System.out.println((++count) + ". " + entry.getValue() + " - " + entry.getKey());
+            if (count >= 5) break;
+        }
+    }
 }
