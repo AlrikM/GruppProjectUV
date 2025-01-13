023bf44167156676cbabc4c15513dca2b844f95d
OlleHellsten <olle411@hotmail.com>
2025-01-13 09:36:16 +0100

    added java doc to the Highscore class

--- src/main/java/Highscore.java
+++ src/main/java/Highscore.java
@@ -1,17 +1,19 @@
 import java.util.*;
 
+// skapar en TreeMap som passar bra för en lista med namn och resultat
 public class Highscore {
     public TreeMap<Integer, String> scores;
-
+// ändrar så att största talet är först
     public Highscore() {
         scores = new TreeMap<>((Collections.reverseOrder()));
 
     }
+    // metod som lägger in i TreeMap:en
     public void addScore(String name, int score) {
         scores.put(score, name);
     }
 
-
+    // loopar fem gånger så vi får topp fem resultat och skriver ut
     public void displayTopScores() {
         System.out.println("Top 5 Scores:");
         int count = 0;
