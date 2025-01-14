0824fa0308853efbff426c3d8891d0577bd3f472
dedu23 <dedu23@student.bth.se>
2025-01-14 11:24:40 +0100

    made simple MathQuizTest

--- src/test/java/MathQuizTest.java
+++ src/test/java/MathQuizTest.java
@@ -1,4 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.Scanner;
+
 import static org.junit.jupiter.api.Assertions.*;
 class MathQuizTest {
-  
+
+    @Test
+    void testGetQuizScore() {
+        MathQuiz mathQuiz = new MathQuiz();
+        assertEquals(0, mathQuiz.getQuizScore());
+
+    }
 }
